1a9c1ba515f6e3eb158f95f8154831e6a055aa5d
Matthias <m.geslin974@gmail.com>
2023-10-24 16:23:41 +0200

    App created
    Import components:
    -top
    -foot
    
    Router links:
    -Home
    -About
    -Creations

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import TheTop from './components/TheTop.vue'
+import TheFoot from './components/TheFoot.vue';
+</script>
+
+<template>
+  <header>
+    <section class="wrapper">
+      <TheTop msg="🛁 MatHub" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/creations">Creations</RouterLink>
+      </nav>
+    </section>
+  </header>
+
+  <RouterView />
+  
+  <TheFoot />
+</template>
