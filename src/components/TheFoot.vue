3a4e801dec1ff4ba05aa1ba196447317a1fb7a80
Matthias <m.geslin974@gmail.com>
2023-10-24 16:36:26 +0200

    Footer template created

---
+++ src/components/TheFoot.vue
@@ -0,0 +1,10 @@
+<template>
+    <footer>
+        <ul>
+            <li><a href="#"><i class="fa-brands fa-x-twitter"></i> Twitter</a></li>
+            <li><a href="#"><i class="fa-brands fa-github"></i> Github</a></li>
+            <li><a href="#"><i class="fa-brands fa-linkedin"></i> linkedin</a></li>
+            <li><a href="#"><i class="fa-regular fa-envelope"></i><RouterLink to="/mail"> Mail</RouterLink></a></li>
+        </ul>
+    </footer>
+</template>
