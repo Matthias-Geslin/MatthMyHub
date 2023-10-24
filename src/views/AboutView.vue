98d7eb0ad419e59d4c5262ec936e3eabb0a3564e
Matthias <m.geslin974@gmail.com>
2023-10-24 16:34:53 +0200

    About view template created

---
+++ src/views/AboutView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import AboutTempHub from '../components/AboutTempHub.vue'
+</script>
+<template>
+  <section>
+    <AboutTempHub 
+      defineImg="CV.pdf" 
+      defineAlt="This is my current updated CV."
+    />
+  </section>
+</template>
