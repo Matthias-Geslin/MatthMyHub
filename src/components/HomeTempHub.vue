faa1d35bc9e833fa6ca722d09887b56a26c6e342
Matthias <m.geslin974@gmail.com>
2023-10-27 13:47:46 +0200

    Created home content

---
+++ src/components/HomeTempHub.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HomeTemp from './HomeTemp.vue'
+</script>
+
+<template>
+  <HomeTemp>
+    <template #heading>Knowledge must and have to be kept at all costs. Life is an never ending experience.</template>
+    <template #content>My work will be shown in here, fun or serious ones. Web langage used in here, HTML5, CSS3 (Scss), JS (VueJs) & PHP.</template>
+  </HomeTemp>
+</template>
