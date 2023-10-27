d672748d892307b10584386ca0875aae2ac6a3a9
Matthias <m.geslin974@gmail.com>
2023-10-27 13:40:05 +0200

    Created image template
    Default image template
    props defined src & alt

---
+++ src/components/TheImageTemp.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    defineSrc: {
+        type: String
+  },
+    defineAlt: {
+        type: String
+    }
+})
+</script>
+
+<template>
+    <img class="defineImg" :src="defineSrc" :alt="defineAlt">
+</template>
