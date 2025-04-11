b40d08848687c8a41ce61c70e167d8be267dbd0b
Matthias <m.geslin974@gmail.com>
2025-04-11 16:36:25 +0200

    Update
    Slight edit

--- src/components/HomeTempHub.vue
+++ src/components/HomeTempHub.vue
@@ -10,7 +10,7 @@ import HomeTemp from './HomeTemp.vue'
 
     <template #content>
       <article class="baseContent home">
-        <p>My work will be shown in here, fun or serious ones. Web langage used in here, HTML5, CSS3 (Scss) & JS (VueJs). There will be no PHP here, a MVC project alike will be made soon to manage more dynamic content.</p>
+        <p>My work will be shown in here, fun or serious ones. Web langage used in here, HTML5, CSS3 (Scss) & JS (VueJs).</p>
         <p>Useful informations are in the other pages of the website, <b><RouterLink to="/about"><u>About</u></RouterLink></b> and <b><RouterLink to="/creativity"><u>Creativity</u></RouterLink></b>.</p>
         <p>Feel free to contact me if you are in need, there are many contact links at the bottom of the page.</p>
       </article>
