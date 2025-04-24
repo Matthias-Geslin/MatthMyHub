0a76fee3b329ac9d45f20090525a233dcad930cc
Matthias <m.geslin974@gmail.com>
2025-04-24 17:48:12 +0200

    Update X link
    Switched link

--- src/components/TheFoot.vue
+++ src/components/TheFoot.vue
@@ -1,7 +1,7 @@
 <template>
     <footer>
         <ul>
-            <li><a href="https://x.com/matth_moi_ca"><i class="fa-brands fa-x-twitter"></i> Twitter</a></li>
+            <li><a href="https://x.com/Matthiasgeslin"><i class="fa-brands fa-x-twitter"></i> Twitter</a></li>
             <li><a href="https://github.com/Matthias-Geslin"><i class="fa-brands fa-github"></i> Github</a></li>
             <li><a href="https://www.linkedin.com/in/matthias-geslin"><i class="fa-brands fa-linkedin"></i> Linkedin</a></li>
             <li><a href="mailto:contact.matthmoica@gmail.com"><i class="fa-regular fa-envelope"></i>Mail</a></li>
