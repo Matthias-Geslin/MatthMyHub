5903a83a4aa238d49428c22dda1449bfc3891a7b
Matthias <m.geslin974@gmail.com>
2025-04-29 21:27:29 +0200

    Update file
    import i18n, and it's functions
    Reworked images array, clearer with names for each items
    removehttp() for url links in images
    Replaced content with $t values i18n json
    menu & figure vfor more functionnal

--- src/components/CreativityTemp.vue
+++ src/components/CreativityTemp.vue
@@ -1,20 +1,36 @@
 <script setup>
-import { ref, reactive, computed, pushScopeId } from 'vue'
+import { reactive, onMounted } from 'vue'
+import { useI18n } from 'vue-i18n';
+const { t, locale } = useI18n();
+import i18n from '../i18n';
 
 const images = reactive([
-  ['professional', 'project_01.jpg', 'Image of project 1, to learn the html and css. Make a front page of the Webagency.', 'Webagency', '', 'Basics of the Html and Css.'],
-  ['professional', 'project_03.jpg', 'Image of project 3', 'JavaScript Learning', '','Learn the Javascript via multiple aspects and functions (eg. Slider, Maps, Canvas & Ajax).'],
-  ['professional', 'project_04.jpg', 'Image of project 4', 'Blog Jean Forteroche', '','Learn the concept of MVC in PHP and created a blog about that use case.'],
-  ['creative', 'procreate_01.png', 'Screenshot from Procreate', 'Procreate', 'Twitch Graphism Panels for contact, links.'],
-  ['creative', 'procreate_02.png', 'Screenshot from Procreate', 'Procreate', 'MMC Logo for my web domain.'],
-  ['devFyi', 'devFyi_01.png', 'Screenshot of a website I made', 'MatthMyWebsite', 'https://matthmoiça.fr', 'For Livestream and YouTube.'],
-  ['devFyi', 'devFyi_02.png', 'Screenshot of a Discord server', 'MatthMoiÇa Discord', 'https://discord.gg/HFxq2UVeqa', 'Server Discord of MatthMoiÇa, server made for the community, fans and whoever wants to join. Made for experimentations purposes.'],
-  ['devFyi', 'devFyi_03.png', 'Screenshot of a Bot Integration and the configuration', 'MatthMyBot', 'https://github.com/Matthias-Geslin/MatthMyBot', "MatthMyBot a bot dedicated for MatthMoiÇa's discord server, made by me and for experimentations purposes."]
-]) 
+  {    category: 'professional', file: 'project_01.jpg', alt: 'Image of project 1', titleKey: 'Webagency', descKey: 'creativity.gallery.project01', nakedUrl: 'https://github.com/Matthias-Geslin/Projet_1_Webagency', url: 'https://github.com/Matthias-Geslin/Projet_1_Webagency'  },
+  {    category: 'professional', file: 'project_03.jpg', alt: 'Image of project 3', titleKey: 'JavaScript', descKey: 'creativity.gallery.project03', nakedUrl: 'https://github.com/Matthias-Geslin/Project_3_Js', url: 'https://github.com/Matthias-Geslin/Project_3_Js'  },
+  {    category: 'professional', file: 'project_04.jpg', alt: 'Image of project 4', titleKey: 'Blog Jean Forteroche', descKey: 'creativity.gallery.project04', nakedUrl: 'https://github.com/Matthias-Geslin/Project_4_Blog', url: 'https://github.com/Matthias-Geslin/Project_4_Blog'  },
+  {    category: 'professional', file: 'pro_01.png', alt: 'Image of a professional one-page', titleKey: "L'HÉROZ'O", descKey: 'creativity.gallery.pro01', nakedUrl: 'https://www.lherozo.fr', url: 'https://www.lherozo.fr'  },
+  {    category: 'creative', file: 'procreate_01.png', alt: 'Screenshot from Procreate', titleKey: 'Procreate', descKey: 'creativity.gallery.procreatePanels'  },
+  {    category: 'creative', file: 'procreate_02.png', alt: 'Screenshot from Procreate', titleKey: 'Procreate', descKey: 'creativity.gallery.procreateLogo'  },
+  {    category: 'devFyi', file: 'devFyi_01.png', alt: 'Screenshot of a website I made', titleKey: 'MatthMyStream', descKey: 'creativity.gallery.livestream', nakedUrl: 'https://stream.matthmoiça.fr', url: 'https://stream.matthmoiça.fr'  },
+  {    category: 'devFyi', file: 'devFyi_02.png', alt: 'Screenshot of a Discord server', titleKey: 'MatthMoiÇa Discord', descKey: 'creativity.gallery.discordServer', nakedUrl: 'https://discord.gg/HFxq2UVeqa', url: 'https://discord.gg/HFxq2UVeqa'  },
+  {    category: 'devFyi', file: 'devFyi_03.png', alt: 'Screenshot of a Bot Integration and the configuration', titleKey: 'MatthMyBot', descKey: "creativity.gallery.discordBot", nakedUrl: 'https://github.com/Matthias-Geslin/MatthMyBot', url: 'https://github.com/Matthias-Geslin/MatthMyBot'  }
+])
+
+function removeHttps() {
+  images.forEach(image => {
+    if (image.nakedUrl) {
+      image.nakedUrl = image.nakedUrl.replace(/^https?:\/\/(www\.)?/, '');
+    }
+  });
+}
 
 function imagesShown() {
   return images.length > 0 ? images.length : '';
 }
+
+onMounted(() => {
+  removeHttps();
+})
 </script>
 
 <template>
@@ -23,23 +39,22 @@ function imagesShown() {
     <input class="hide" id="creative" name="creativity" type="radio" value="creativity">
     <input class="hide" id="devFyi" name="creativity" type="radio" value="devFyi">
 
-  <h3>Original creator of those {{ imagesShown() }} projects below.</h3>
+  <h3>{{ $t('creativity.title.firstPart') }} {{ imagesShown() }} {{ $t('creativity.title.secondPart') }}</h3>
 
   <menu class="menu">
-    <label for="all">All</label>
-    <label class="" for="professional">Professional</label>
-    <label class="" for="creative">Creative</label>
-    <label class="" for="devFyi">Dev - WIP</label>
+    <label for="all">{{ $t('creativity.menu.labelAll') }}</label>
+    <label for="professional">{{ $t('creativity.menu.labelPro') }}</label>
+    <label for="creative">{{ $t('creativity.menu.labelCrea') }}</label>
+    <label for="devFyi">{{ $t('creativity.menu.labelDev') }}</label>
   </menu>
 
   <figure class="gallery">
-    <figure v-for="image in images" :class="'all ' + image[0]">   
-      <img :id="image[0]" :name="image[3]" :src="'/images/' + image[1]" :alt="image[2]" />
+    <figure v-for="image in images" :class="'all ' + image.category">   
+      <img :id="image.category" :name="image.titleKey" :src="'/images/' + image.file" :alt="image.alt" />
       <figcaption>
-        <h3>{{ image[3] }}</h3>
-        <p v-if="image[0] == 'creative'">{{ image[4] }}</p>
-        <a v-if="image[0] == 'devFyi'" :href="image[4]">{{  image[4] }}</a>
-        <p>{{ image[5] }}</p>
+        <h3>{{ image.titleKey }}</h3>
+        <p>{{ t(image.descKey) }}</p>
+        <a v-if="image.url" :href="image.url">{{  image.nakedUrl }}</a>
       </figcaption>
     </figure>
   </figure>
