00fc8e4228a363dd347e25d66243a3ac8f241d85
Matthias <m.geslin974@gmail.com>
2025-05-27 17:48:37 +0200

    Update
    Removed useless html tags and moved the lang box

--- src/components/AboutTempHub.vue
+++ src/components/AboutTempHub.vue
@@ -42,22 +42,22 @@ import AboutTemp from './AboutTemp.vue'
         <article class="baseContent">
           <p>{{ $t('about.content.myExperience') }}</p>
           <p>{{ $t('about.content.websiteObjective') }}<a href="https://stream.matthmoiça.fr/"><i class="fa-brands fa-twitch"></i>Twitch</a></p>
-        </article>
 
-        <div class="box baseContent">
-          <ul class="ul-lang">
-            <li>{{ $t('about.langages') }}</li>
-            <li class="li-lang" v-for="value in listItems">
-              <div class="lang">
-                <div class="bar">
-                  <span class="percent">{{ value[0] }}: {{ value[1] }}%</span>
-                  <div :class="'progress ' + value[2]" v-bind:style="{'width': value[1] + '%' }">
+          <div class="box">
+            <ul class="ul-lang">
+              <li>{{ $t('about.langages') }}</li>
+              <li class="li-lang" v-for="value in listItems">
+                <div class="lang">
+                  <div class="bar">
+                    <span class="percent">{{ value[0] }}: {{ value[1] }}%</span>
+                    <div :class="'progress ' + value[2]" v-bind:style="{'width': value[1] + '%' }">
+                    </div>
                   </div>
                 </div>
-              </div>
-            </li>
-          </ul>
-        </div>
+              </li>
+            </ul>
+          </div>
+        </article>
       </template>
   </AboutTemp>
 
